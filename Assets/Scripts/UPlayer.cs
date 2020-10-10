d2c773066209f5a19b35b341d7dc81465744f2a8
Collin Argo <cargo@insiten.com>
2020-10-10 12:55:26 -0400

    Added player object and target objects to basic scene with controls and behavior

---
+++ Assets/Scripts/UPlayer.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace MyFirstGame.Assets.Scripts
+{
+    public class UPlayer : MonoBehaviour
+    {
+        public float speed;
+        public float spin;
+        public List<UTarget> targets;
+
+        private Dictionary<UTarget, Coroutine> _targetsFleeing = new Dictionary<UTarget, Coroutine>();
+
+        // Start is called before the first frame update
+        protected void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        protected void Update()
+        {
+            _MoveSelf();
+            _DestroyTarget();
+        }
+
+        private void _MoveSelf()
+        {
+            var up = Input.GetKey(KeyCode.UpArrow);
+            if (up)
+            {
+                transform.position += transform.forward * speed * Time.deltaTime;
+                // Vector3 position = transform.position;
+                // position.z -= speed * Time.deltaTime;
+                // transform.position = position;
+            }
+            var down = Input.GetKey(KeyCode.DownArrow);
+            if (down)
+            {
+                transform.position -= transform.forward * speed * Time.deltaTime;
+                //Vector3 position = transform.position;
+                //position.z += speed * Time.deltaTime;
+                //transform.position = position;
+            }
+            var left = Input.GetKey(KeyCode.LeftArrow);
+            if (left)
+            {
+                transform.Rotate(new Vector3(0, -spin * Time.deltaTime, 0));
+                //Vector3 position = transform.position;
+                //position.x += speed * Time.deltaTime;
+                //transform.position = position;
+            }
+            var right = Input.GetKey(KeyCode.RightArrow);
+            if (right)
+            {
+                transform.Rotate(new Vector3(0, spin * Time.deltaTime, 0));
+                //Vector3 position = transform.position;
+                //position.x -= speed * Time.deltaTime;
+                //transform.position = position;
+            }
+        }
+
+        private void _DestroyTarget()
+        {
+            var newTargets = new List<UTarget>();
+            targets.ForEach((target) =>
+            {
+                Debug.Log("target");
+                var distance = Vector3.Distance(transform.position, target.transform.position);
+                if (distance <= 1)
+                {
+                    Debug.Log("target destroyed");
+                    //target.frozen = !target.frozen;
+                    Destroy(target.gameObject);
+                    return;
+                }
+                else if (distance < 10 && distance < 20)
+                {
+                    Debug.Log("target fleeing");
+                    if (!_targetsFleeing.ContainsKey(target))
+                    {
+                        var runAwayCoroutine = StartCoroutine(target.RunAway(() => transform.position));
+                        _targetsFleeing.Add(target, runAwayCoroutine);
+                    }
+                }
+                else if (distance > 20)
+                {
+                    Debug.Log("target stop fleeing");
+                    if (_targetsFleeing.TryGetValue(target, out var runAwayCoroutine))
+                    {
+                        StopCoroutine(runAwayCoroutine);
+                        _targetsFleeing.Remove(target);
+                    }
+                }
+                Debug.Log("target survived");
+                newTargets.Add(target);
+            });
+            targets = newTargets;
+        }
+    }
+}
---
+++ Assets/Scripts/UTarget.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace MyFirstGame.Assets.Scripts
+{
+    public class UTarget : MonoBehaviour
+    {
+
+        public bool frozen;
+        public float speed;
+        public float spin;
+
+        protected void Start()
+        {
+            StartCoroutine(_Bounce());
+            StartCoroutine(_Spin());
+        }
+
+        public IEnumerator RunAway(Func<Vector3> getPosition)
+        {
+            while (true)
+            {
+                var positionOther = getPosition();
+                var positionSelf = transform.position;
+                var direction = (positionSelf - positionOther).normalized;
+                direction.y = 0;
+                positionSelf += direction * speed * Time.deltaTime;
+                transform.position = positionSelf;
+
+                yield return null;
+            }
+        }
+
+        private IEnumerator _Spin()
+        {
+            while (true)
+            {
+                transform.Rotate(new Vector3(spin * speed * Time.deltaTime, spin * speed * Time.deltaTime, spin * speed *  Time.deltaTime));
+                yield return null;
+            }
+        }
+
+        private IEnumerator _Bounce()
+        {
+            var y = transform.position.y;
+            var value = 0f;
+
+            while (true)
+            {
+                var position = transform.position;
+                value += Time.deltaTime;
+                position.y = y + Mathf.Sin(value);
+                transform.position = position;
+
+                if (frozen)
+                {
+                    yield return new WaitUntil(() => !frozen);
+                }
+                else
+                {
+                    yield return null;
+                }
+
+            }
+
+            //Debug.Log("Here!");
+            //yield return null;
+            //Debug.Log("And here!");
+        }
+
+        //public int bounce;
+        //private bool moveUp = false;
+        //private int maxHeight = 3;
+        //private int minHeight = 1;
+
+        //private float _y;
+        //private float _value;
+
+        //public void Update()
+        //{
+        //    _y = transform.position.y;
+        //    _Animate();
+        //}
+
+        //private void _Animate()
+        //{
+        //    //var position = transform.position;
+        //    //_value += Time.deltaTime;
+        //    //position.y = _y + Mathf.Sin(_value);
+        //    //transform.position = position;
+
+        //    Vector3 position = transform.position;
+        //    var timeBounce = Time.deltaTime * bounce;
+        //    if (moveUp)
+        //    {
+        //        position.y += timeBounce;
+        //    }
+        //    else
+        //    {
+        //        position.y -= timeBounce;
+        //    }
+        //    Debug.Log(new { position, timeBounce, moveUp });
+        //    transform.position = position;
+        //    if (transform.position.y > maxHeight)
+        //    {
+        //        moveUp = false;
+        //    }
+        //    else if (transform.position.y < minHeight)
+        //    {
+        //        moveUp = true;
+        //    }
+        //}
+    }
+}
